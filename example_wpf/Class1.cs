96f43b455c3250b189cc19409db49b69e64480e8
ВЛАДИСЛАВ ВЛАДИСЛАВ <Vladislav290403@gmail.com>
2024-07-23 22:54:46 +0400

    Добавьте файлы проекта.

---
+++ example_wpf/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MVVM
+{
+    public class Calculate : INotifyPropertyChanged
+    {
+        private int first_num;
+        private int second_num;
+        private int total_num;
+
+        public int First_num
+        {
+            get { return first_num; }
+            set
+            {
+                first_num = value;
+                OnPropertyChanged("First_num");
+            }
+        }
+        public int Second_num
+        {
+            get { return second_num; }
+            set
+            {
+                second_num = value;
+                OnPropertyChanged("Second_num");
+            }
+        }
+        public int Total_num
+        {
+            get { return total_num; }
+            set { total_num = value; }
+        }
+
+        public void set_result() {
+            total_num = first_num + second_num;
+            OnPropertyChanged("Total_num");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ example_wpf/Class2.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using System.Security.Policy;
+
+namespace MVVM
+{
+    public class ApplicationViewModel : INotifyPropertyChanged
+    {
+        private Calculate my_class;
+
+        // команда добавления нового объекта
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                  (addCommand = new RelayCommand(obj =>
+                  {
+                     
+                      my_class.set_result();
+                  }));
+            }
+        }
+
+        public Calculate SelectedPhone
+        {
+            get { return my_class; }
+            set
+            {
+                my_class = value;
+                OnPropertyChanged("SelectedPhone");
+            }
+        }
+
+        public ApplicationViewModel()
+        {
+
+            my_class = new Calculate { };    
+           
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
